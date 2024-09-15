08ae51ef6957b6613d6f9464f68928f59df8046c
TheNarrator42 <43051137+TheNarrator42@users.noreply.github.com>
2024-09-15 14:09:23 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Boxing Boards and Battle Maps: Yet Another Nerdy Improv Game Show")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
